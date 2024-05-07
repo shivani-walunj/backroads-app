a5f1e34a29452f22f350fe30937343f329f31104
shivani-walunj <shivaniiwalunj@gmail.com>
2024-05-07 12:13:29 +0530

    first commit

---
+++ src/components/PageLink.js
@@ -0,0 +1,11 @@
+const PageLink = ({ id, href, text, itemClass }) => {
+  return (
+    <li key={id}>
+      <a href={href} className={itemClass}>
+        {text}
+      </a>
+    </li>
+  );
+};
+
+export default PageLink;
